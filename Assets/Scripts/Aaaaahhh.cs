18e5d91379d0b4b788240c1040c53a773d083119
Rhylee Zoudlik <rjzoudlik@gmail.com>
2021-02-14 20:20:04 +1100

    too much stuff to be able to write down in the amount of time it takes for me to get bored.

--- Assets/Scripts/Aaaaahhh.cs
+++ Assets/Scripts/Aaaaahhh.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Aaaaahhh : MonoBehaviour
 {
@@ -8,10 +10,18 @@ public class Aaaaahhh : MonoBehaviour
     public Rigidbody2D rb;
     public float speed;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.AddForce(new Vector2(0f,-200f));
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/CameraSpriteFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpriteFollower : MonoBehaviour
+{
+    private Vector3 offset;
+
+    public GameObject plr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - plr.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plr.transform.position + offset;
+    }
+}
